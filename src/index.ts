c2d5af6fa54e5df8201ce5b3ff28d6fb978be51e
Matteo <46478501+m-gallesio@users.noreply.github.com>
2023-04-27 23:21:38 +0200

    Exposed toImage.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { toSvgText } from "./buildSvg";
 import { downloadRaster, downloadSvg } from "./download";
-import { toCanvas, toRasterBlob, toRasterDataUri, toSvgDataUri } from "./render";
+import { toCanvas, toImage, toRasterBlob, toRasterDataUri, toSvgDataUri } from "./render";
 
 export const exportSvg = Object.freeze({
     toSvgText,
     toSvgDataUri,
+    toImage,
     toCanvas,
     toRasterDataUri,
     toRasterBlob,
